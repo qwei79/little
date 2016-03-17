dbc22b85d5ae38085fc946940c8a1edb4dc63fe3
憨态书生 <qwei@msn.com>
2016-03-17 13:20:42 +0800

    终于调对了。

--- javascripts/scrolling.js
+++ javascripts/scrolling.js
@@ -1,38 +1,48 @@
-window.onload=function(){
+function startMove(){
   var sScrolling=document.getElementsByClassName("sscrolling"),i;
   for(i=0; i<sScrolling.length; i++){
     var sUl=sScrolling[i].getElementsByTagName("ul")[0];
     var sLi=sUl.getElementsByTagName("li");
-    startMove(i,sLi[0].offsetHeight);
+    startMoveX(i,sLi[0].offsetHeight);
   }
 }
-function startMove(i,sSheight){
+function startMoveX(i,sSheight){
   var sScrolling=document.getElementsByClassName("sscrolling")[i];
   var sUl,sLi,sUlw=0;
   sUl=sScrolling.getElementsByTagName("ul")[0];
   sLi=sUl.getElementsByTagName('li');
+  /* 获取一组内容 */
   var Wc=sUl.innerHTML;
+  /* 首次定义ul宽度 */
   sUl.innerHTML+=sUl.innerHTML;
   for(var ii=0; ii<sLi.length; ii++){
     sLi[ii].style.height = sSheight;
     sUlw+=sLi[ii].offsetWidth + 10;
   }
-  sUl.style.width=sUlw;
-  var Wd=sScrolling.offsetWidth*2-sUl.offsetWidth;
-  for(Wd; Wd>0; Wd=Wd-sUl.offsetWidth){
+  sUl.style.width=sUlw+'px';
+  /* 获取一组宽度 */
+  WcW=sUl.offsetWidth/2;
+  /* 添加内容以完全覆盖并且有一组完全在显示区域外 */
+  var Wd=sScrolling.offsetWidth+WcW-sUl.offsetWidth;
+  for(Wd; Wd>0; Wd=Wd-WcW){
     sUl.innerHTML=sUl.innerHTML + Wc;
   }
-  
+  /* 计算添加完成后的ul宽度 */
   for(var ii=0; ii<sLi.length; ii++){
     sLi[ii].style.height = sSheight;
     sUlw+=sLi[ii].offsetWidth + 10;
   }
-  sUl.style.width=sUlw;
-  
-  sScrolling.style.height=sSheight + 10;
+  sUl.style.width=sUlw+'px';
+  /* 设置显示区域高度为首个li的高度 */
+  sScrolling.style.height=sSheight + 10+'px';
+  /* 滚动函数 */
   function timer(){
-    if(sUl.offsetLeft<-sUl.offsetWidth/2){
-      sUl.style.left=10;
+    /* 当内容滚过一组的宽度的时候，则回到开始位置，为了不卡顿设置为“-1” */
+    if(sUl.offsetLeft<-WcW){
+      console.log(sUl.offsetLeft);
+      console.log(WcW);
+      sUl.style.left=-1+'px';
+      console.log(sUl.offsetLeft);
     }
     sUl.style.left=sUl.offsetLeft-1+'px';
   }
