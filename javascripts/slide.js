f743368f12ae938f8771557039fef2f67f5a8960
憨态书生 <qwei@msn.com>
2016-03-25 12:20:04 +0800

    真的好了！！！

--- javascripts/slide.js
+++ javascripts/slide.js
@@ -66,7 +66,6 @@ function slideMove(index){
 var pro=document.getElementById("slide-pro");
 var pro_ul=pro.getElementsByTagName("ul")[0];
 var pro_li=pro_ul.getElementsByTagName("li");
-console.log("测试li1"+pro_li);
 //设定li的宽度等于盒子宽度，并且隐藏起来，仅显示首个li
 pro_li_width=pro.offsetWidth;
 for (var i = pro_li.length - 1; i >= 0; i--) {
@@ -86,7 +85,7 @@ for (var i = pro_btn_icon.length - 1; i >= 0; i--) {
   pro_btn_icon[i].onclick = function(num){
     return function(){
       proSlideMove(num);
-      auto_index=num+1;
+      pro_auto_index=num+1;
     }
   }(i);
 };
@@ -96,10 +95,14 @@ var pro_btn_r=document.getElementById("btnR-pro");
 var pro_slide_index=0;
 pro_btn_l.onclick = function(){
   pro_slide_index--;
+  if (pro_slide_index==-1) {pro_slide_index=pro_li.length-1;};
+  pro_auto_index=pro_slide_index+1;
   proSlideMove(pro_slide_index);
 }
 pro_btn_r.onclick = function(){
   pro_slide_index++;
+  if (pro_slide_index==pro_li.length) {pro_slide_index=0;};
+  pro_auto_index=pro_slide_index+1;
   proSlideMove(pro_slide_index);
 }
 //自动播放
@@ -111,6 +114,7 @@ function proSlideAutoMove() {
   if(pro_auto_index == pro_btn_icon.length) {
     pro_auto_index =0;
   }
+  pro_slide_index = pro_auto_index;
   proSlideMove(pro_auto_index);
   pro_auto_index += 1;
 }
@@ -122,44 +126,57 @@ function proSlideMove(pro_index){
       pro_index_now = i;
     };
   };
+  console.log("接收值是"+ pro_index);
+  if (pro_index<0) {pro_index=pro_li.length-1;pro_slide_index=pro_index;};
+  if (pro_index>pro_li.length-1) {pro_index=0;pro_slide_index=pro_index;};
+  /*
+  pro_li[pro_index_now].style.left=pro_li_width+"px";
+  clearInterval(rightMoveAuto);
+  clearInterval(leftMoveAuto);
+  */
+  var speed=10;//设置速度，数字越大，速度越快
   if(pro_index<pro_index_now) {
-    if (pro_index<0) {pro_index=pro_li.length-1;pro_slide_index=pro_index;};
+    //if (pro_index<0) {pro_index=pro_li.length-1;pro_slide_index=pro_index;};
     pro_li[pro_index].style.display="block";
     pro_li[pro_index].style.left=0+"px";
     function rightMove(){
-      pro_li[pro_index_now].style.left=pro_li[pro_index_now].offsetLeft+1+"px";
-      pro_li[pro_index].style.left=pro_li[pro_index].offsetLeft+1+"px";
-      console.log(pro_li[pro_index].offsetLeft);
-      if(pro_li[pro_index].offsetLeft==pro_li_width){
+      pro_li[pro_index_now].style.left=pro_li[pro_index_now].offsetLeft+speed+"px";
+      pro_li[pro_index].style.left=pro_li[pro_index].offsetLeft+speed+"px";
+      if(pro_li[pro_index].offsetLeft>=pro_li_width-speed&&pro_li[pro_index].offsetLeft<=pro_li_width+speed){
         clearInterval(rightMoveAuto);
+        pro_li[pro_index].style.left=pro_li_width + "px";
         for(var n=0;n<pro_li.length;n++) {
           pro_li[n].style.display = (n==pro_index) ? ("block") : ("none");
         }
       };
     }
-    var rightMoveAuto=setInterval(rightMove,1);
+    var rightMoveAuto;
+    rightMoveAuto=setInterval(rightMove,1);
   }
   if(pro_index>pro_index_now) {
-    if (pro_index>pro_li.length-1) {pro_index=0;pro_slide_index=pro_index;};
+    //if (pro_index>pro_li.length-1) {pro_index=0;pro_slide_index=pro_index;};
     pro_li[pro_index].style.display="block";
     pro_li[pro_index].style.left=pro_li_width*2+"px";
     function leftMove(){
-      pro_li[pro_index_now].style.left=pro_li[pro_index_now].offsetLeft-1+"px";
-      pro_li[pro_index].style.left=pro_li[pro_index].offsetLeft-1+"px";
-      if(pro_li[pro_index].offsetLeft==pro_li_width){
-        console.log(pro_li[pro_index].offsetLeft);
-        console.log(pro_li_width);
+      pro_li[pro_index_now].style.left=pro_li[pro_index_now].offsetLeft-speed+"px";
+      pro_li[pro_index].style.left=pro_li[pro_index].offsetLeft-speed+"px";
+      if(pro_li[pro_index].offsetLeft>=pro_li_width-speed&&pro_li[pro_index].offsetLeft<=pro_li_width+speed){
         clearInterval(leftMoveAuto);
+        pro_li[pro_index].style.left=pro_li_width + "px";
         for(var n=0;n<pro_li.length;n++) {
           pro_li[n].style.display = (n==pro_index) ? ("block") : ("none");
         }
       };
     }
-    var leftMoveAuto=setInterval(leftMove,1);
+    var leftMoveAuto;
+    leftMoveAuto=setInterval(leftMove,1);
   }
   for(var n=0;n<pro_btn_icon.length;n++) {
     pro_btn_icon[n].className = ""; 
   }
   pro_btn_icon[pro_index].className = "active";
-
+  console.log("当前是" + pro_index);
+  console.log("前一张是" + pro_index_now);
+  console.log("自动准备是" + pro_auto_index);
+  console.log("方向准备是" + pro_slide_index);
 }
